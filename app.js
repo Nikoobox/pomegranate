f2f1c9697d9abf300820ffe90536da468641d3f7
Nikolay <nikoobox@gmail.com>
2020-11-28 16:21:58 -0500

    first commit

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.send('Hello Nikolay!');
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;    
 
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
