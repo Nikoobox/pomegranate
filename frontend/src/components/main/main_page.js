89b8963a94f04dfb643de12947da42dd0f411edf
Nikolay <nikoobox@gmail.com>
2020-11-30 14:16:22 -0500

    testing initial access

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,10 +5,10 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>A Twitter Clone</h1>
+                <h1>Pomegranate</h1>
                 <footer>
-                    Copyright &copy; 2019 Chirper
-        </footer>
+                    Footer goes here
+                </footer>
             </div>
         );
     }
