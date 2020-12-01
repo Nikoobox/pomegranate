e590fb2362e514f28e5e5a6bd7d9b8a191a47468
Nikolay <nikoobox@gmail.com>
2020-12-01 11:25:52 -0500

    app js changes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,14 +8,17 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import KitchenFormContainer from './kitchen/kitchen_form_container';
 
-// const path = require('path');
+const express = require("express");
+const app = express();
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     })
-// }
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 const App = () => (
     <div>
