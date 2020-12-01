2fafce2f6a0b8696ddd7379a0ff9dd2fb1598384
Nikolay <nikoobox@gmail.com>
2020-12-01 11:40:06 -0500

    frontend app jsx

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const kitchens = require('./routes/api/kitchens');
 
+const express = require("express");
+const app = express();
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
