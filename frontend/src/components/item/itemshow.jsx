c6082ddbd3cee921c75f5c1ddea3dc42e15a2797
italo <italo.lujanpedreschi@gmail.com>
2020-12-05 11:07:54 -0800

    Expiration date on item card does not show if not set

--- frontend/src/components/item/itemshow.jsx
+++ frontend/src/components/item/itemshow.jsx
@@ -11,13 +11,19 @@ class ItemShow extends React.Component {
             <div className='item-quantity'>You have {item.quantity} items</div>
 
         const date = new Date(item.expirationDate);
+        let dateDiv;
+        if (date.toDateString() === "Wed Dec 31 1969") {
+            dateDiv = "No expiration date.";
+        } else {
+            dateDiv = `This product will expire on ${date.toDateString()}.`;
+        }
         return (
            <div className='item-box'>
                 <Link to={`item/${item._id}/edit`} className='item-box-link'>
                     <div className='item-name'>{item.name}</div>
                     {quantities}
                 </Link>
-                <div className='item-box-expiration'>This product will expire on {date.toDateString()}.</div>     
+                <div className='item-box-expiration'>{dateDiv}</div>
            </div>
         )
     }
