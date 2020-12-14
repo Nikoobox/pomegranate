a458406b300d0678b80415ae2055e50c05c8846f
italo <italo.lujanpedreschi@gmail.com>
2020-12-14 13:07:33 -0800

    Commit to make new branch

--- frontend/src/components/item/item_show_container.js
+++ frontend/src/components/item/item_show_container.js
@@ -1,8 +1,7 @@
 import { connect } from 'react-redux';
-import {getItem } from '../../actions/item_actions';
+import { getItem } from '../../actions/item_actions';
 import ItemShow from './itemshow';
 // import { openModal } from '../../actions/modal_actions';
-
 const mapStateToProps = (state, ownProps) => {
     const id = ownProps.match.params.itemId
     const item = state.items.data[id]
@@ -11,20 +10,12 @@ const mapStateToProps = (state, ownProps) => {
         item
     };
 };
-
 const mapDispatchToProps = (dispatch) => {
     return {
-<<<<<<< HEAD
-        getItem: (itemId) => dispatch(getItem(itemId)),
-        openModal: (type, data) => dispatch(openModal(type, data))
-=======
         getItem: itemId => dispatch(getItem(itemId)),
-        // openModal: modal => dispatch(openModal(modal))
->>>>>>> e23dafaa9d0e64c90a26db5662ea818a8bf2c7f2
     }
 }
-
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(ItemShow); 
+)(ItemShow);
--- frontend/src/components/item/itemshow.jsx
+++ frontend/src/components/item/itemshow.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 class ItemShow extends React.Component {
-
     render() {
         console.log(this.props);
         const { item } = this.props;
-        const quantities = item.quantity === 1 ? 
+        const quantities = item.quantity === 1 ?
             <div className='item-quantity'>You have {item.quantity} item</div>
             :
             <div className='item-quantity'>You have {item.quantity} items</div>
-
         const date = new Date(item.expirationDate);
         let dateDiv;
         if (date.toDateString() === "Wed Dec 31 1969") {
@@ -24,23 +21,13 @@ class ItemShow extends React.Component {
         }
         return (
            <div className='item-box'>
-                <Link to={`/item/${item._id}/edit`} className='item-box-link' onClick={() => this.props.openModal('edit')} />
-
-                {/* <div className='item-box-link' onClick={() => this.props.openModal('edit')} > */}
-
-                {/* <button className='item-box-link' onClick={() => {
-                debugger
-                   return this.props.openModal('edit', item._id)
-                }}> */}
-                    
+                <div className='item-box-link' onClick={() => this.props.openModal('edit')} >
                     <div className='item-name'>{item.name}</div>
                     {quantities}
-                {/* </button> */}
-                {/* </div> */}
+                </div>
                 {dateDiv}
            </div>
         )
     }
 }
-
 export default ItemShow;
