0d4c85f81dc819bdad95b66df3af21b7c47606f8
Amgaa <amgaa.khosbayar@gmail.com>
2021-01-06 14:33:34 -0800

    updated recipe page

--- frontend/src/components/recipe/recipe.jsx
+++ frontend/src/components/recipe/recipe.jsx
@@ -5,7 +5,7 @@ class Recipe extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
+        // this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
@@ -18,41 +18,11 @@ class Recipe extends React.Component {
         this.props.clearCurrentRecipeState();
     }
 
-    handleClick() {
-        if(this.props.items) {
-            this.props.recipe.extendedIngredients.map(ingre => {
-
-                Object.values(this.props.items).map( item =>{
-
-                    if(ingre.name.includes(item.name) && item.quantity >= ingre.amount) {
-                        const cooked = document.querySelector('.cooked');
-                        let word;
-                        if(ingre.amount >= 2) {
-                            word = 'portions';
-                        } else {
-                            word = 'portion';
-                        }
-                        cooked.innerText = `You used ${ingre.amount} ${word} of ${ingre.name} from your kitchen`;
-                        item.quantity = `${item.quantity - ingre.amount}`;
-                        
-                    } else if(ingre.name.includes(item.name) && item.quantity < ingre.amount) {
-                        const error = document.querySelector('.not-enough');
-                        error.innerText = `Sorry, you do not have enough ${item.name}, you need at least ${ingre.amount}`;
-                    } else {
-                        const warning = document.querySelector('.warning');
-                        warning.innerText = ``;
-                    }
-                    item.quantity = `${item.quantity}`;
-                    this.props.editItem(item);
-                })
-            })
-            this.props.clearRecipeState();
-        }
-    }
 
     render() {
-
+        
         if(this.props.recipe) {
+
             return (
                 <div className='show-recipe-container'>
                     <div className='recipe-box'>
@@ -65,15 +35,15 @@ class Recipe extends React.Component {
                                 <div className='title'><span>Recipe Name: </span> {this.props.recipe.title} </div> 
                                 <div className='time'><span>Cooking Time: </span>{this.props.recipe.readyInMinutes} minutes</div> 
                                 <div className='instructions'><span>Instructions: </span> {this.props.recipe.instructions}</div>
-
-                                <div className="cooked"></div>
-                                <div className="not-enough"></div>
-                                <div className="warning"></div>
+                                <div> Required Ingredients: 
+                                    {this.props.recipe.extendedIngredients.map(ingre => `${ingre.name} - ${ingre.amount},`)}
+                                </div>
+                                <div> Items in kitchen: 
+                                    {Object.values(this.props.items).map( item => `${item.name} - ${item.quantity}, please update inventory manually`)}
+                                </div>
                                 <div className='btn-container'>
                                     <button onClick={this.handleClick} className='cook'>Cook this recipe</button>
-
                                     <button onClick={() => { this.props.history.push('/browse') }} className='jump-to-recipes'>Jump back to recipes</button>
-
                                 </div>
                             </div>
 
