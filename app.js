9b9810e14c7454db883feefb3e86e59dcbd4dcd8
Nikolay <nikoobox@gmail.com>
2021-02-02 19:07:05 -0500

    login error testing

--- app.js
+++ app.js
@@ -15,26 +15,31 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     })
-// }
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname +  '/index.html');
-})
+    
+    app.use(passport.initialize());
+    require("./config/passport")(passport);
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use("/api/users", users);
+    app.use("/api/items", items);
+    
 
-app.use(passport.initialize());
-require("./config/passport")(passport);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use("/api/users", users);
-app.use("/api/items", items);
+    app.use(express.static('frontend/build'));
+    // app.get('/', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    // })
+
+        
+    app.get('*', (req, res) => {
+        res.redirect("/#");
+    })
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
